a2298c779c5b1c540e7d1f4cb9b6a56d49786a18
Atikur Rahman <atik.bd08@gmail.com>
2014-12-30 00:45:56 +0600

    add LocationDetailsViewController class

---
+++ LocationDetailsViewController.swift
@@ -0,0 +1,27 @@
+//
+//  LocationDetailsViewController.swift
+//  MyLocations
+//
+//  Created by Atikur Rahman on 12/30/14.
+//  Copyright (c) 2014 Atikur Rahman. All rights reserved.
+//
+
+import UIKit
+
+class LocationDetailsViewController: UITableViewController {
+    @IBOutlet weak var descriptionTextView: UITextView!
+    @IBOutlet weak var categoryLabel: UILabel!
+    @IBOutlet weak var latitudeLabel: UILabel!
+    @IBOutlet weak var longitudeLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    @IBAction func done() {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    @IBAction func cancel() {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+
+}
