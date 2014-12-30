b7a266ba67d2f57fa8500d72026833ac4b184415
Atikur Rahman <atik.bd08@gmail.com>
2014-12-31 00:00:58 +0600

    putting location info into tag location screen

--- LocationDetailsViewController.swift
+++ LocationDetailsViewController.swift
@@ -7,8 +7,16 @@
 //
 
 import UIKit
+import CoreLocation
 
-class LocationDetailsViewController: UITableViewController {
+private let dateFormatter: NSDateFormatter = {
+    let formatter = NSDateFormatter()
+    formatter.dateStyle = .MediumStyle
+    formatter.timeStyle = .ShortStyle
+    return formatter
+}()
+
+class LocationDetailsViewController: UITableViewController, UITextViewDelegate {
     @IBOutlet weak var descriptionTextView: UITextView!
     @IBOutlet weak var categoryLabel: UILabel!
     @IBOutlet weak var latitudeLabel: UILabel!
@@ -16,12 +24,74 @@ class LocationDetailsViewController: UITableViewController {
     @IBOutlet weak var addressLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
     
+    var descriptionText = ""
+    
+    var coordinate = CLLocationCoordinate2D(latitude: 0, longitude: 0)
+    var placemark: CLPlacemark?
+    
     @IBAction func done() {
+        println("Description: \(descriptionText)")
         dismissViewControllerAnimated(true, completion: nil)
     }
     
     @IBAction func cancel() {
         dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        descriptionTextView.text = descriptionText
+        categoryLabel.text = ""
+        
+        latitudeLabel.text = String(format: "%.8f", coordinate.latitude)
+        longitudeLabel.text = String(format: "%.8f", coordinate.longitude)
+        
+        if let placemark = placemark {
+            addressLabel.text = stringFromPlacemark(placemark)
+        } else {
+            addressLabel.text = "No Address Found"
+        }
+        
+        dateLabel.text = formatDate(NSDate())
+    }
+    
+    func stringFromPlacemark(placemark: CLPlacemark) -> String {
+        return
+            "\(placemark.subThoroughfare) \(placemark.thoroughfare), " +
+            "\(placemark.locality), " +
+            "\(placemark.administrativeArea) \(placemark.postalCode)," +
+            "\(placemark.country)"
+    }
+    
+    func formatDate(date: NSDate) -> String {
+        return dateFormatter.stringFromDate(date)
+    }
+    
+    // MARK: - UITableViewDelegate
+    
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        if indexPath.section == 0 && indexPath.row == 0 {
+            return 88
+        } else if indexPath.section == 2 && indexPath.row == 2 {
+            addressLabel.frame.size = CGSize(width: view.bounds.size.width - 115, height: 10000)
+            addressLabel.sizeToFit()
+            addressLabel.frame.origin.x = view.bounds.size.width - addressLabel.frame.size.width - 15
+            return addressLabel.frame.size.height + 20
+        } else {
+            return 44
+        }
+    }
+    
+    // MARK: - UITextViewDelegate
+    
+    func textView(textView: UITextView, shouldChangeTextInRange range: NSRange, replacementText text: String) -> Bool {
+        descriptionText = (textView.text as NSString).stringByReplacingCharactersInRange(range, withString: text)
+        return true
+    }
+    
+    func textViewDidEndEditing(textView: UITextView) {
+        descriptionText = textView.text
     }
 
 }
