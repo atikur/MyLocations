6c3f16c0446cf4d66523371d45141d78800ed0a4
Atikur Rahman <atik.bd08@gmail.com>
2015-01-01 11:34:50 +0600

    add LocationCell class

---
+++ MyLocations/LocationCell.swift
@@ -0,0 +1,31 @@
+//
+//  LocationCell.swift
+//  MyLocations
+//
+//  Created by Atikur Rahman on 1/1/15.
+//  Copyright (c) 2015 Atikur Rahman. All rights reserved.
+//
+
+import UIKit
+
+class LocationCell: UITableViewCell {
+
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+
+    func configureForLocation(location: Location) {
+        if location.locationDescription.isEmpty {
+            descriptionLabel.text = "(No Descript)"
+        } else {
+            descriptionLabel.text = location.locationDescription
+        }
+        
+        if let placemark = location.placemark {
+            addressLabel.text =
+                "\(placemark.subThoroughfare) \(placemark.thoroughfare)," +
+                "\(placemark.locality)"
+        } else {
+            addressLabel.text = String(format: "Lat: %.8f, Long: %.8f", location.latitude, location.longitude)
+        }
+    }
+}
